cd418dfea168c88846b9e6ee2c34c2c822794681
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-19 12:46:00 -0500

    add working timer heart loss win and lose page and functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,38 +2,99 @@ $(function () {
   'use strict';
   // var flip=true;
   var storage = null;
-  var foot = $("input");
+  // var foot = $("input");
   // var arrayHolder = [];
+  var start = new Date().getTime();
+
+
+
+
+
+  function appendZero(z) {
+      return ( z < 10 ? "0" : "" ) + z;
+    }
+
+  setInterval(function() {
+    var counter = Math.round((new Date - start) / 1000);
+
+    var hours = Math.floor(counter / 3600);
+    counter = counter % 3600;
+
+    var minutes = Math.floor(counter / 60);
+    counter = counter % 60;
+
+    var seconds = Math.floor(counter);
+
+    hours = appendZero(hours);
+    minutes = appendZero(minutes);
+    seconds = appendZero(seconds);
+
+    var timer = hours + ":" + minutes + ":" + seconds;
+
+    $('.timer').text(timer);
+  }, 1000);
+
+
+
+
+  // seconds = 0;
+  // minutes = 0;
+  // hours = 0;
+  //
+  // setInterval(function() {
+  //     $('.Timer').text((new Date - start) / 1000 + " Seconds");
+  // }, 1000);
+
+
+
+
 
   $(".card").on('click', function () {
 
     if($(".clicked").length % 2 === 0){
       storage=$(this).attr("id");
-      console.log(storage);
+      // console.log(storage);
     }
 
     if($(".clicked").length < 19){
       $(this).addClass("clicked");
-      console.log($(".clicked").length);
+      console.log($(".clicked2").length);
+      console.log($(".heart").length);
+    }
+
+    if($(".clicked2").length > 17){
+      $(".modal2").addClass("showing");
+    }
+
+    if($(".heart").length === 0){
+      $(".modal").addClass("showing");
     }
 
     if($(".clicked").length % 2 === 0){
-      console.log("IM COUNTING!");
+      // console.log("IM COUNTING!");
       if($(this).attr("id") === storage){
         storage=null;
+        ($(".clicked").addClass("clicked2"));
         // arrayHolder.length = $(".clicked").length;
         // console.log(arrayHolder.length)
       }
+
       else{
         storage=null;
+        $(".heart:last-child").remove();
         for(var i = 0; i<$(".clicked").length; i++){
-          $(".clicked").removeClass("clicked");
-          $('.modal').addClass('showing');
+          //New Version!
+          setTimeout(function() {
+            $(".clicked").removeClass("clicked");
+          }, 1000);
+
+
+          // $(".clicked").removeClass("clicked"); Original version;
         }
       }
   }
     else{
-      console.log("IM NOT COUNTING!")
+      // console.log("IM NOT COUNTING!")
     }
 
 
@@ -46,15 +107,7 @@ $(function () {
 
 
 
-
-
-
-
-
-
-
-
-
+    //  Method unworkable
     // if($("input:checked").length = 1){
     //    console.log("NO!!!!!")
     //   if($(this).attr("id") === storage){
@@ -94,7 +147,7 @@ $(function () {
 
 
 
-
+    //Interesting Concept
     // $(this).addClass("clicked");
     // storage = $(this);
     //
@@ -145,3 +198,51 @@ $(function () {
 
   });
 });
+
+  // var hours = 0;
+  // var minutes = 0;
+  // var seconds = 0;
+  // var pastHours = undefined;
+  // var pastMinutes = undefined;
+  // var pastSeconds = undefined;
+  // var timeUpdate;
+  //
+  //
+  // window.onload = function updateTime(pastHours, pastMinutes, pastSeconds){
+  //       var startTime = new Date();    // fetch current time
+  //
+  //       timeUpdate = setInterval(function () {
+  //           var timeElapsed = new Date().getTime() - startTime.getTime();    // calculate the time elapsed in milliseconds
+  //
+  //           // calculate hours
+  //           hours = parseInt(timeElapsed / 1000 / 60 / 60) + pastHours;
+  //
+  //           // calculate minutes
+  //           minutes = parseInt(timeElapsed / 1000 / 60) + pastMinutes;
+  //           if (minutes > 60) minutes %= 60;
+  //
+  //           // calculate seconds
+  //           seconds = parseInt(timeElapsed / 1000) + pastSeconds;
+  //           if (seconds > 60) seconds %= 60;
+  //
+  //           // set the stopwatch
+  //           setStopwatch(hours, minutes, seconds);
+  //
+  //           console.log("f");
+  //
+  //       }, 1000); // update time in stopwatch after every 25ms
+  //
+  //   }
+
+    // Set the time in stopwatch
+    // window.onload = function setStopwatch(hours, minutes, seconds){
+    //     $("#hours").html(hours);
+    //     $("#minutes").html(minutes);
+    //     $("#seconds").html(seconds);
+    // }
+
+    // Prepend zeros to the digits in stopwatch
+    // window.onload = function prependZero(time, length) {
+    //     time = new String(time);    // stringify time
+    //     return new Array(Math.max(length - time.length + 1, 0)).join("0") + time;
+    // }
